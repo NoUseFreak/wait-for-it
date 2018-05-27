69e997343b5c92ecd142674b8d8090a3e16dcebe
Dries De Peuter <dries@nousefreak.be>
2018-05-27 23:22:36 +0200

    Simplify installer

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 [![Build Status](https://travis-ci.org/NoUseFreak/wait-for-it.svg?branch=master)](https://travis-ci.org/NoUseFreak/wait-for-it)
 
+`wait-for-it` is a cli tool to check if a service is available. It was build to run between starting docker services
+and running integration tests.
+
+The tool is plugin based, only the plugins for your specific cases are installed.
+
 # Install
 
 ```bash
-curl -L -o /usr/local/bin/wait-for-it https://github.com/NoUseFreak/wait-for-it/releases/download/0.0.6/`uname`_wait-for-it
-chmod +x /usr/local/bin/wait-for-it
+curl -sL http://bit.ly/get-wait-for-it | bash
 ```
 
 # Usage
---
+++ scripts/get.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Usage: curl https://raw.githubusercontent.com/NoUseFreak/wait-for-it/master/scripts/get.sh | bash
+
+
+
+get_latest_release() {
+
+  curl --silent "https://api.github.com/repos/NoUseFreak/wait-for-it/releases/latest" |
+    grep '"tag_name":' |
+    sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+download() {
+
+  curl -Ls -o /usr/local/bin/wait-for-it https://github.com/NoUseFreak/wait-for-it/releases/download/$1/`uname`_wait-for-it
+}
+echo "Looking up latest release"
+RELEASE=$(get_latest_release)
+
+echo "Downloading package"
+$(download RELEASE)
+
+echo "Making executable"
+sudo chmod +x /usr/local/bin/wait-for-it
